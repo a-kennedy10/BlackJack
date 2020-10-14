1b98941dd138da39afa15ffe7d87f8bd38f9fc4b
Kaleb  Carrizoza <kalebcarrizoza@Kalebs-MacBook-Pro.local>
2020-10-14 16:25:15 -0700

    add model and view controller files

---
+++ BlackJackApp/BlackJackApp/Model/Controllers/ModelController/CardController.swift
@@ -0,0 +1,33 @@
+//
+//  CardController.swift
+//  BlackJackApp
+//
+//  Created by Kaleb  Carrizoza on 10/14/20.
+//  Copyright © 2020 Alex Kennedy. All rights reserved.
+//
+
+import Foundation
+
+class CardController {
+    
+    //MARK: - Properties
+    static let shared = CardController()
+    var cards: [Card] = []
+    
+    
+    func starterhand() {
+        
+    }
+    
+    
+    
+    func update() {
+        
+    }
+   
+    
+    
+    
+    
+    
+}//end of class
